3a97f010753e5f3cf147079bc0a9499eb14103c7
Rowan Christmas <rowanxmas@gmail.com>
2024-11-06 15:19:59 -0800

    hmm

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -5,7 +5,6 @@ import './style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
-  base: '/blog/',
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
